97445f6b50adba1f77fe0a77b062d7f5d94ff9d2
Leandro Sabus <leandro.sabus@gmail.com>
2023-01-30 15:39:54 -0300

    top cards added

--- src/components/TopCards.jsx
+++ src/components/TopCards.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export const TopCards = () => {
     return (
-        <div>TopCards</div>
+        <div className='flex flex-col gap-y-8 p-6 md:flex-row justify-center md:justify-between '>
+            <div className='flex flex-row gap-4 justify-between w-full p-2'>
+                <div className='flex flex-col  '>
+                    <span className='font-bold'>$7.426</span>
+                    <span className='text-slate-600 font-semibold'>Daily revenue</span>
+                </div>
+
+                <span className='bg-green-200 text-green-700 rounded-lg flex items-center p-1'>+18%</span>
+            </div>
+            <div className='flex flex-row gap-4 justify-between w-full p-2 '>
+                <div className='flex flex-col'>
+                    <span className='font-bold'>$1.820.485</span>
+                    <span className='text-slate-600 font-semibold'>YTD revenue</span>
+                </div>
+
+                <span className='bg-green-200 text-green-700 rounded-lg flex items-center p-1'>+18%</span>
+            </div>
+            <div className='flex flex-row gap-4 justify-between w-full p-2 '>
+                <div className='flex flex-col'>
+                    <span className='font-bold'>$17.510</span>
+                    <span className='text-slate-600 font-semibold'>Customers</span>
+                </div>
+
+                <span className='bg-green-200 text-green-700 rounded-lg flex items-center p-1'>+18%</span>
+            </div>
+
+        </div>
     )
 }
